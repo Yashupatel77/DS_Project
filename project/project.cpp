ac18c9f21cd9afe084725cca5d01651345a8c4bb
yashu <pyashu793@gmail.com>
2024-08-11 13:57:52 -0400

    Created the function to generate hash values, created the struct for the parcel and hash table

--- project/project.cpp
+++ project/project.cpp
@@ -1,8 +1,27 @@
-// project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#define HASH_TABLE_SIZE 127
+#define MAX_COUNTRY_LENGTH 20
 
-#include <iostream>
+typedef struct Parcel {
+    char* destination;
+    int weight;
+    float value;
+    struct Parcel* LeftChild;
+    struct Parcel* RightChild;
+} Parcel;
 
-int main()
-{}
+typedef struct HashTable {
+    Parcel* root[HASH_TABLE_SIZE];
+}HashTable;
 
+unsigned long GenerateHash(char* str) {
+    unsigned long hash = 5381;
+    int c;
+
+    while (c = *str++)
+        hash = ((hash << 5) + hash) + c;
+    hash %= HASH_TABLE_SIZE;
+    return hash;
+}
